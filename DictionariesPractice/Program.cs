8483a746ef0d3a0eda35a71bf33adebbc02c461a
Frank Nasseri <franknasseri57@gmail.com>
2017-07-13 09:56:25 -0400

    adding animal dictionary

--- DictionariesPractice/Program.cs
+++ DictionariesPractice/Program.cs
@@ -40,6 +40,69 @@ namespace DictionariesPractice
             //    Console.WriteLine(drink.Value);
             //}
 
+            //Create a dictionary for a thearter coat check with 10 elements
+            //the key will be a number and the value will be the coat style
+            //print all the elements to the console
+
+            //Dictionary<int, string> coatCheck = new Dictionary<int, string>();
+            //coatCheck.Add(1, "Leather Jacket");
+            //coatCheck.Add(2, "Bomber Jacket");
+            //coatCheck.Add(3, "Wind Breker");
+            //coatCheck.Add(4, "Winter Jacket");
+            //coatCheck.Add(5, "Rain Coat");
+            //coatCheck.Add(6, "Trench Coat");
+            //coatCheck.Add(7, "Puffer Jacket");
+            //coatCheck.Add(8, "Northface");
+            //coatCheck.Add(9, "Hoodie");
+            //coatCheck.Add(10, "Zip-up");
+
+            //foreach (KeyValuePair<int,string> jacket in coatCheck)
+            //{
+            //    Console.WriteLine(jacket.Key);
+            //    Console.WriteLine(jacket.Value);
+            //}
+
+            //Car valet with 10 cars the key will be a customers last name and the value will be the car make
+            //print all the elements to the console
+            //Dictionary<string, string> carValet = new Dictionary<string, string>()
+            //{
+            //    {"Nasseri", "Maserati" },
+            //    {"Thomas", "Ford" },
+            //    {"Russo", "Jeep" },
+            //    {"Calabrese", "Mercedes" },
+            //    {"Marous", "Range Rover" },
+            //    {"Rowe", "Volkswagon" },
+            //    {"Phelps", "Mazda" },
+            //    {"Argie", "Audi" },
+            //    {"Rubin", "Volvo" },
+            //    {"Bundy", "Toyota" }
+            //};
+            //foreach (KeyValuePair<string,string> car in carValet)
+            //{
+            //    Console.WriteLine(car);
+            //}
+
+            //create a dictionary of 10 zoo animals. the key will be the animal type. the value will be the number of animals
+            //print the animal withthe highest quantity to the console.
+            //remove the zoo animal with the lowest count. use the Remove()
+
+            Dictionary<string, int> zooAnimals = new Dictionary<string, int>()
+            {
+                {"Monkeys", 11 },
+                {"Zebras", 6 },
+                {"Lions", 5 },
+                {"Tigers", 5 },
+                {"Bears", 7 },
+                {"Gorillas", 4 },
+                {"Camels", 3 },
+                {"zlephants", 2 },
+                {"Hippos", 9 },
+                {"Panters", 1 }
+            };
+            Console.WriteLine(zooAnimals.Keys.Max()); 
+            //Console.WriteLine(zooAnimals.Keys."Monk);
+
+
         }
     }
 }
